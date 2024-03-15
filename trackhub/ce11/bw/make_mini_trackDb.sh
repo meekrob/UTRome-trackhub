93db408e80ea1eac5c7073a7c55d03f54a303844
David <davidcking.inbox@gmail.com>
2024-03-15 15:15:48 -0600

    changed script to explictly say "bigWig"

--- trackhub/ce11/bw/make_mini_trackDb.sh
+++ trackhub/ce11/bw/make_mini_trackDb.sh
@@ -6,7 +6,7 @@ do
 
     minmax=$(bigWigInfo $fname | cut -f 2 -d ' ' | tail -3 | head -2 | tr '\n' ' ')
     echo "track $Acc"
-    echo "type bigwig 0 50000"
+    echo "type bigWig 0 50000"
     echo "bigDataUrl bw/$fname"
     echo "shortLabel $Desc"
     echo "longLabel $Desc"
